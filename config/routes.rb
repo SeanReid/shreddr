c9d6f61ddca555045214f3c3b995378b49efcff0
Sean Reid <sreid757@gmail.com>
2014-07-13 19:20:27 -0500

    new form issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   root to: 'message#index'
   resources :user
   resources :message
+  post '/message/new' => 'message#new', as: 'messages'
+  get 'message/index' => 'message#index'
   # get "message/:id/edit" => "message#edit"
-  get "message/new" => "message#new"
   # get ":name" => "user#show", as: "message_status"
 
 end
